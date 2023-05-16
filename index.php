899e83c5f56664149bcb84cca0b974035005807a
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-16 15:53:15 +0200

    create fx variabili and function construct

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+
+
+class movie 
+{
+
+public $id;
+
+public $title;
+
+public $original_title;
+
+public $nationality;
+
+public $date;
+
+public $vote;
+
+public $image;
+
+public function __construct($title, $original_title, 
+$nationality, $date, $vote, $image)
+{
+  
+    $this->title = $title;
+    $this->original_title = $original_title;
+    $this->nationality = $nationality;
+    $this->date = $date;
+    $this->vote = $vote;
+    $this->image = $image;
+
+}
+
+}
+
+
+
+?>
+
